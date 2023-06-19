461f827e9999b420303c75188d6d1ed200326c0e
banerjri <banerjri@mail.uc.edu>
2023-06-18 22:14:35 -0500

    Project done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int intialBalance = 5000;
+        double interestRate = 0.17;
+        double interestMonth1;
+        double interestMonth2;
+        double balanceAfterMonth1;
+        interestMonth1 = intialBalance * interestRate;
+        System.out.println("The interest due after one month = " + interestMonth1);
+        interestMonth2 = (intialBalance + interestMonth1) * interestRate;
+        System.out.println("The interest due after two month = " + interestMonth2);
     }
 }
